691d3cc6c4c0bfc125f30462771a09f9a1bc7d37
Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
2011-12-29 10:02:23 +0000

    unnützes load entfernt

--- hooks/class.tx_mklib_hooks_t3lib_tceforms_getSingleFieldClass.php
+++ hooks/class.tx_mklib_hooks_t3lib_tceforms_getSingleFieldClass.php
@@ -30,7 +30,6 @@
  * benötigte Klassen einbinden
  */
 require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
-tx_rnbase::load('tx_mklib_util_ServiceRegistry');
 
 /**
  * Hooks for auto-updating search indices
