f056a7cba6d6877b1c0c3957439dd2684d095e82
Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
2012-01-20 10:33:54 +0000

    abstrakte observer klasse, zur generischen unterstützung von unterstützen interfaces, die an execute übergeben werden

---
+++ abstract/class.tx_mklib_abstract_Observer.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * 	@package tx_mkjjk
+ *  @subpackage tx_mkjjk_srv
+ *  @author Hannes Bochmann
+ *
+ *  Copyright notice
+ *
+ *  (c) 2010 Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+/**
+ * benötigte Klassen einbinden
+ */
+require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
+tx_rnbase::load('tx_mklib_interface_IObserver');
+
+/**
+ * generischer observer.
+ * einfach die execute methode bereitstellen und das
+ * eventuell unterstütze interface, welches genutzt wird
+ * um die daten des observable abzufragen
+ *
+ * @package tx_mkjjk
+ * @subpackage tx_mkjjk_srv
+ */
+abstract class tx_mklib_abstract_Observer implements tx_mklib_interface_IObserver {
+
+	/**
+	 * wenn das observable nicht diese interface implementiert
+	 * dann wird execute() nicht ausgeführt.
+	 * wenn nicht auf ein interface geprüft werden soll dann einfach
+	 * leer lassen.
+	 * Beispiel:
+	 * protected $sSupportedInterface = tx_mkextension_interface_ForSomething
+	 * die klasse einfach angeben. nicht als string!
+	 *
+	 * @var const
+	 */
+	protected $sSupportedInterface;
+
+	/**
+	 * kann nicht überschrieben werden!!!
+	 * (non-PHPdoc)
+	 * @see tx_mklib_interface_IObserver::notify()
+	 */
+	final public function notify(tx_mklib_interface_IObservable $oObservable) {
+		//wird nur ein bestimmtes interface unterstützt?
+		if($this->sSupportedInterface && !$oObservable instanceof $this->sSupportedInterface)
+			return;
+		$this->execute($oObservable);
+	}
+
+	/**
+	 * führt die eigentiche arbeit des jeweiligen
+	 * observers aus
+	 *
+	 * @param tx_mklib_interface_IObservable $oObservable
+	 * @return void
+	 */
+	abstract protected function execute(tx_mklib_interface_IObservable $oObservable);
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/mkjjk/srv/class.tx_mkjjk_srv_JJKInterface.php'])	{
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/mkjjk/srv/class.tx_mkjjk_srv_JJKInterface.php']);
+}
+
+?>
