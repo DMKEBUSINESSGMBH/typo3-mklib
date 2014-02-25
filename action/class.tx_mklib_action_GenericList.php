6dd4ff1b8474605a8793395c81bf993aee4c4abe
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-02-25 13:50:14 +0100

    Bugfix

--- action/class.tx_mklib_action_GenericList.php
+++ action/class.tx_mklib_action_GenericList.php
@@ -43,7 +43,7 @@ tx_rnbase::load('tx_rnbase_filter_BaseFilter');
 class tx_mklib_action_GenericList extends tx_rnbase_action_BaseIOC {
 
 	protected $confIdExtended = 'default.';
-	
+
 	/**
 	 *
 	 * @param tx_rnbase_IParameters $parameters
@@ -55,30 +55,30 @@ class tx_mklib_action_GenericList extends tx_rnbase_action_BaseIOC {
 		// confIdExtended setzen
 		$this->confIdExtended = $configurations->get($this->getConfId().'extendedConfId');
 		$this->confIdExtended = $this->confIdExtended ? $this->confIdExtended : 'default';
-		
+
 		$confId = $this->getExtendedConfId();
-		
+
 		// Filter erstellen.
 		/* @var $filter tx_rnbase_filter_BaseFilter */
 		$filter = tx_rnbase_filter_BaseFilter::createFilter($parameters, $configurations, $viewData, $confId.'filter.');
 		$fields = $options = array();
-		
+
 		// Searcher instanzieren. Konfiguriert wird er über die options['searchdef']
 		/* @var $searcher tx_rnbase_util_SearchGeneric */
 		tx_rnbase::load('tx_rnbase_util_SearchBase');
 		$searcher = tx_rnbase_util_SearchBase::getInstance('tx_rnbase_util_SearchGeneric');
-		
+
 		// Dem Filter den Searcher übergeben, fall er diese Möglichkeit bietet.
 		if(method_exists($filter, 'setSearcher')) {
 			$filter->setSearcher($searcher);
 		}
-		
+
 		// Suche initialisieren und nur ausführen wenn der Filter es erlaubt.
 		if ($doSearch = $filter->init($fields, $options)) {
-			
+
 			// Soll ein PageBrowser verwendet werden?
 			if ($configurations->get($confId.'pagebrowser')) {
-				$pbOptions = array('searchcallback'=> array($this->getSearcher(), 'search'));
+				$pbOptions = array('searchcallback'=> array($searcher, 'search'));
 				$cbOptions['pbid'] = ($var = $configurations->get($confId.'pagebrowser.cbid'))
 						? $var : 'pb'.$this->confIdExtended;
 				$filter->handlePageBrowser($configurations,
@@ -86,7 +86,7 @@ class tx_mklib_action_GenericList extends tx_rnbase_action_BaseIOC {
 					$fields, $options, $pbOptions
 				);
 			}
-			
+
 			// Soll ein CharBrowser verwendet werden?
 			if ($configurations->get($confId.'charbrowser')) {
 				// optionen sammeln
@@ -102,18 +102,18 @@ class tx_mklib_action_GenericList extends tx_rnbase_action_BaseIOC {
 					$fields, $options, $cbOptions
 				);
 			}
-			
+
 			// items besorgen.
 			$items = $searcher->search($fields, $options);
 		}
 		else $items = array();
-		
+
 		$viewData->offsetSet('items', $items);
 		$viewData->offsetSet('searched', $doSearch);
 
 		return null;
 	}
-	
+
 	/**
 	 * Liefert die Erweiterte ConfId für den View
 	 * @return string
@@ -121,7 +121,7 @@ class tx_mklib_action_GenericList extends tx_rnbase_action_BaseIOC {
 	public function getExtendedConfId() {
 		return $this->getConfId().$this->confIdExtended.'.';
 	}
-	
+
 	/**
 	 * Liefert den Default-Namen des Templates. Über diesen Namen
 	 * wird per Konvention auch auf ein per TS konfiguriertes HTML-Template
@@ -140,7 +140,7 @@ class tx_mklib_action_GenericList extends tx_rnbase_action_BaseIOC {
 	public function getViewClassName() {
 		return 'tx_mklib_view_GenericList';
 	}
-	
+
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/mklib/action/class.tx_mklib_action_GenericList.php'])	{
