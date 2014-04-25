1285b6ace952fba5e28ed12642333737014865d7
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-04-25 13:21:58 +0200

    Leerzeichen mit Tabs getauscht

--- scheduler/ext_localconf.php
+++ scheduler/ext_localconf.php
@@ -4,22 +4,22 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 
-if (TYPO3_MODE=='BE')    {
+if (TYPO3_MODE=='BE')	{
 
-    // Register information for the test and sleep tasks
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_mklib_scheduler_cleanupTempFiles'] = array(
-    	'extension'        => 'mklib',
-    	'title'            => 'LLL:EXT:mklib/scheduler/locallang.xml:scheduler_cleanupTempFiles_name',
-    	'description'      => 'LLL:EXT:mklib/scheduler/locallang.xml:scheduler_cleanupTempFiles_taskinfo',
+	// Register information for the test and sleep tasks
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_mklib_scheduler_cleanupTempFiles'] = array(
+		'extension'		=> 'mklib',
+		'title'			=> 'LLL:EXT:mklib/scheduler/locallang.xml:scheduler_cleanupTempFiles_name',
+		'description'	  => 'LLL:EXT:mklib/scheduler/locallang.xml:scheduler_cleanupTempFiles_taskinfo',
 		'additionalFields' => 'tx_mklib_scheduler_cleanupTempFilesFieldProvider'
-    );
+	);
 
-    // prüft ob scheduler schon seit längerer zeit hängen
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_mklib_scheduler_SchedulerTaskFreezeDetection'] = array(
-    	'extension'        => 'mklib',
-    	'title'            => 'LLL:EXT:mklib/scheduler/locallang.xml:scheduler_SchedulerTaskFreezeDetection_name',
-    	'description'      => 'LLL:EXT:mklib/scheduler/locallang.xml:scheduler_SchedulerTaskFreezeDetection_taskinfo',
+	// prüft ob scheduler schon seit längerer zeit hängen
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_mklib_scheduler_SchedulerTaskFreezeDetection'] = array(
+		'extension'		=> 'mklib',
+		'title'			=> 'LLL:EXT:mklib/scheduler/locallang.xml:scheduler_SchedulerTaskFreezeDetection_name',
+		'description'	  => 'LLL:EXT:mklib/scheduler/locallang.xml:scheduler_SchedulerTaskFreezeDetection_taskinfo',
 		'additionalFields' => 'tx_mklib_scheduler_SchedulerTaskFreezeDetectionFieldProvider'
-    );
+	);
 
 }
