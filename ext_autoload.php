618fd836f0f97ff7672bfcf1cc96bdaf2e1c62e2
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2015-12-08 12:31:35 +0100

    AC#235-3372 Pfade im Autoloading gefixed

--- ext_autoload.php
+++ ext_autoload.php
@@ -12,7 +12,10 @@
  * Um die Datei automatisch generieren zu lassen, kann die Extension extdeveval genutzt werden.
  */
 
-$extensionPath = tx_rnbase_util_Extensions::extPath('mklib');
+// wir können weder den Extensionmanager nutzen (auf Grund der verschiedenen TYPO3 Versionen)
+// noch tx_rnbase_util_Extensions (weil nicht geladen). Also nehmen wir den Pfad hart. Dürfte
+// aber kein Problem sein.
+$extensionPath = PATH_typo3conf . 'ext/mklib/';
 return array(
 	// exceptions
 	'tx_mklib_exception_invalidconfiguration' => $extensionPath . 'exception/class.tx_mklib_exception_InvalidConfiguration.php',
