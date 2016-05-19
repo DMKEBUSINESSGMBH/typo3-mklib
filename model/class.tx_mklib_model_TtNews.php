027d106a2b0e2ac71e26e8df6d83761d4aa3ec0a
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2016-05-19 12:18:41 +0200

    AC#420-602 neues Model für tt_news

---
+++ model/class.tx_mklib_model_TtNews.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * 	@package TYPO3
+ *  @subpackage mklib
+ *  @author Hannes Bochmann
+ *
+ *  Copyright notice
+ *
+ *  (c) 2012 Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+/**
+ * benötigte Klassen einbinden
+ */
+
+tx_rnbase::load('tx_rnbase_model_base');
+
+/**
+ * @package TYPO3
+ * @subpackage mklib
+ */
+class tx_mklib_model_TtNews extends tx_rnbase_model_base {
+
+	/**
+	 * (non-PHPdoc)
+	 * @see tx_rnbase_model_base::getTableName()
+	 */
+	public function getTableName() {
+		return 'tt_news';
+	}
+}
