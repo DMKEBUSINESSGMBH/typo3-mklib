94f43d358347d32149498d357af7a154cf17d489
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2017-09-18 15:16:26 +0200

    bugfix use new folder since TYPO3 8.6

--- cli/class.tx_mklib_cli_main.php
+++ cli/class.tx_mklib_cli_main.php
@@ -163,7 +163,7 @@ class tx_mklib_cli_main extends Tx_Rnbase_CommandLine_Controller
      */
     private function flushCacheForTypo6AndHigher()
     {
-        $commonCacheDirectory = tx_rnbase_util_TYPO3::isTYPO87OrHigher() ? 'typo3temp/var/Cache' : 'typo3temp/Cache';
+        $commonCacheDirectory = tx_rnbase_util_TYPO3::isTYPO86OrHigher() ? 'typo3temp/var/Cache' : 'typo3temp/Cache';
         \TYPO3\CMS\Core\Utility\GeneralUtility::flushDirectory(PATH_site . $commonCacheDirectory, true, true);
 
         $cacheManager = tx_rnbase::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager');
