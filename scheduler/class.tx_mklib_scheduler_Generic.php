7022f574cc6bc8530cf07f9de255cc6c98a7af8f
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2019-05-29 12:46:59 +0200

    make $options again private to not loose saved state
    a scheduler task that was saved when the variable was private won't have
    the options read from the serialized object now that it's protected. so
    we revert that
    
    #11

--- scheduler/class.tx_mklib_scheduler_Generic.php
+++ scheduler/class.tx_mklib_scheduler_Generic.php
@@ -51,7 +51,7 @@ abstract class tx_mklib_scheduler_Generic extends Tx_Rnbase_Scheduler_Task
      *
      * @var     array
      */
-    protected $options = array();
+    private $options = array();
 
     /**
      * Extension key, used for devlog.
