a8b83881c0049cc8c18df8c37c275d4814961629
Markus Crasser <markus.crasser@dmk-ebusiness.de>
2022-04-06 14:15:06 +0200

    Require rn_base >= 1.15.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF['mklib'] = [
     'lockType' => '',
     'constraints' => [
         'depends' => [
-            'rn_base' => '1.15.0-',
+            'rn_base' => '1.15.2-',
             'typo3' => '9.5.24-10.4.99',
         ],
         'conflicts' => [],
