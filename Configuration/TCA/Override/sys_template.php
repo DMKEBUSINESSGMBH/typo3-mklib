2d3842269e51de7f80cd986e376e97376f79f747
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2023-04-03 11:47:42 +0200

    Bugfix fix plugin and TS registration

---
+++ Configuration/TCA/Override/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('mklib', 'static/basic/', 'MK Lib - Basics');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('mklib', 'static/development/', 'MK Lib - Development');
---
+++ Configuration/TCA/Override/tt_content.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('TYPO3')) {
+    exit('Access denied.');
+}
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['tx_mklib'] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['tx_mklib'] = 'layout,select_key,pages';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('tx_mklib', 'FILE:EXT:mklib/flexform_main.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    [
+        'LLL:EXT:mklib/locallang_db.php:plugin.mklib.label',
+        'tx_mklib',
+        'EXT:mklib/ext_icon.gif',
+    ],
+    'list_type',
+    'mklib'
+);
